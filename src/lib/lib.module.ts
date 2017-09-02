7f8c87e1ba324c3bb6d511e8189ba1fae0b92603
e-cloud <saintscott119@gmail.com>
2017-09-02 21:40:57 +0800

    refactor(import-order): sort imports in lib.module.ts

--- src/lib/lib.module.ts
+++ src/lib/lib.module.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common'
 import { ModuleWithProviders, NgModule } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { TreeNodeDropSlotComponent } from 'ngx-tree/components/tree-node-drop-slot/tree-node-drop-slot.component'
-import { TreeDragDirective } from 'ngx-tree/directives/tree-drag.directive'
-import { TreeDropDirective } from 'ngx-tree/directives/tree-drop.directive'
-import { TreeDraggingTargetService } from 'ngx-tree/services/tree-dragging-target.service'
 import { TreeLoadingComponent } from './components/tree-loading/tree-loading.component'
 import { TreeNodeChildrenComponent } from './components/tree-node-children/tree-node-children.component'
 import { TreeNodeContentComponent } from './components/tree-node-content/tree-node-content.component'
+import { TreeNodeDropSlotComponent } from './components/tree-node-drop-slot/tree-node-drop-slot.component'
 import { TreeNodeExpanderComponent } from './components/tree-node-expander/tree-node-expander.component'
 import { TreeNodeWrapperComponent } from './components/tree-node-wrapper/tree-node-wrapper.component'
 import { TreeNodeComponent } from './components/tree-node/tree-node.component'
 import { TreeViewportComponent } from './components/tree-viewport/tree-viewport.component'
 import { TreeComponent } from './components/tree/tree.component'
+import { TreeDragDirective } from './directives/tree-drag.directive'
+import { TreeDropDirective } from './directives/tree-drop.directive'
+import { TreeDraggingTargetService } from './services/tree-dragging-target.service'
 
 @NgModule({
     imports: [
